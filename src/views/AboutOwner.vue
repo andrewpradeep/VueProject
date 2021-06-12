9b81170217b0a526a374782d94f892b3b0433b9b
pradeep.j@zohocorp.com <pradeep.j@zohocorp.com>
2021-06-13 04:22:45 +0530

    storybooks updation check

--- src/views/AboutOwner.vue
+++ src/views/AboutOwner.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="about">
-        <h1>Dynamic About page</h1>
+        <h1>Dynamic Story Book</h1>
         <h2> Owner is {{owner}}</h2>
         <h2 v-if="id">Employee id is {{id}}</h2>
     </div>
